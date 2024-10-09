2358779cf0e4b30f02926c224065cdce19cc4228
Saad <78992081+Saad-Naseer@users.noreply.github.com>
2024-10-09 12:40:46 +0200

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
 //const socket = io.connect('http://localhost:5000');
-const socket = io.connect(window.location.origin);
+const socket = io.connect();
 
 
 // Store whether the device is successfully connected
